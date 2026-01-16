3fc5a92d1067f0ad7e619c263a0cd9b5c645b87f
Vaishnavi Patil <vaishnavianilpatil0@gmail.com>
2026-01-16 18:30:35 +0530

    signs.py

--- vision/signs.py
+++ vision/signs.py
@@ -1,4 +1,4 @@
-import cv2
+# import cv2
 from ultralytics import YOLO
 
 model = YOLO("models/signs_best.pt")
@@ -17,29 +17,29 @@ def detect_signs(frame):
 
     return detections
 
-video_path = "testing\video.mp4"   # replace with your video file
-cap = cv2.VideoCapture(video_path)
+# video_path = "testing\video.mp4"   # replace with your video file
+# cap = cv2.VideoCapture(video_path)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+# while cap.isOpened():
+#     ret, frame = cap.read()
+#     if not ret:
+#         break
 
-    detections = detect_signs(frame)
+#     detections = detect_signs(frame)
 
-    # Draw bounding boxes
-    for det in detections:
-        x1, y1, x2, y2 = map(int, det["bbox"])
-        label = f'{det["class"]} {det["confidence"]:.2f}'
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(frame, label, (x1, y1 - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+#     # Draw bounding boxes
+#     for det in detections:
+#         x1, y1, x2, y2 = map(int, det["bbox"])
+#         label = f'{det["class"]} {det["confidence"]:.2f}'
+#         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+#         cv2.putText(frame, label, (x1, y1 - 10),
+#                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
-    cv2.imshow("Sign Detection", frame)
+#     cv2.imshow("Sign Detection", frame)
 
-    # Press 'q' to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#     # Press 'q' to quit
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
